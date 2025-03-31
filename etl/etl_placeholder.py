e2ab456a72783d97136e15945d4f731a9157526f
ianfey <128096734+ianfey@users.noreply.github.com>
2025-03-31 18:42:29 -0400

    Delete etl directory

--- etl/etl_placeholder.py
+++
@@ -1,6 +0,0 @@
-# etl/etl_placeholder.py
-
-# This is a placeholder for your ETL process script.
-# You can use pandas or SQL to clean, join, and load data.
-
-print("ETL script to clean and load IMDb data.")
